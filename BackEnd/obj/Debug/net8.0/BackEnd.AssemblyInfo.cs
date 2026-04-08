9f52e0453dfbeeb3f4065eb82e6c0be3f2bd5c29
AliRhayel10 <alirhayel10@icloud.com>
2026-04-08 11:16:54 +0300

    dsasad

--- BackEnd/obj/Debug/net8.0/BackEnd.AssemblyInfo.cs
+++ BackEnd/obj/Debug/net8.0/BackEnd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1c1360f222d096a20ea52643bf24952897653a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0b8719ae5e61d2d59f37c92c9234b98ee790b0c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
