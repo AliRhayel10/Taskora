ba374d8427966c28649c0a6f818c3c1180bb8d02
AliRhayel10 <alirhayel10@icloud.com>
2026-04-08 17:27:10 +0300

    lewilhefw

--- FrontEnd/src/components/admin/TeamDetailsPage.jsx
+++ FrontEnd/src/components/admin/TeamDetailsPage.jsx
@@ -1,12 +1,21 @@
+import { FiArrowLeft } from "react-icons/fi";
 import "../../assets/styles/admin/team-details-page.css";
 
-export default function TeamDetailsPage({ team }) {
+export default function TeamDetailsPage({ team, onBack }) {
   const title = team?.teamName || "Team";
   const description = team?.description || "No description available.";
 
   return (
     <section className="team-details-page">
       <div className="team-details-page__title-row">
+        
+        <button
+          className="team-details-back-btn"
+          onClick={onBack}
+        >
+          <FiArrowLeft />
+        </button>
+
         <h2>{title}</h2>
         <div className="team-details-page__title-line"></div>
       </div>
