7cf79f18117c53ba5624fd2b931845ffd1a7a792
AliRhayel10 <alirhayel10@icloud.com>
2026-04-24 18:42:17 +0300

    rgre

--- BackEnd/obj/Debug/net8.0/BackEnd.AssemblyInfo.cs
+++ BackEnd/obj/Debug/net8.0/BackEnd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e02e377ec0aa87e6a1fac0e20353c83243a0e91")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d292c2269fb2a9613990aea5e45eea971c28f6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
