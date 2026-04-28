c14736f055bdadacce401f45cd1a0f690d0506bb
AliRhayel10 <alirhayel10@icloud.com>
2026-04-28 11:12:03 +0300

    grs

--- FrontEnd/src/components/teamleader/TeamLeaderTeamSection.jsx
+++ FrontEnd/src/components/teamleader/TeamLeaderTeamSection.jsx
@@ -5,7 +5,7 @@ export default function TeamLeaderTeamSection() {
     <section className="team-leader-team-section">
       <div className="team-leader-team-section__title-row">
         <h2>Team</h2>
-        <div className="team-leader-team-section__title-line"></div>
+        <div className="team-leader-team-section__title-line" />
       </div>
     </section>
   );
